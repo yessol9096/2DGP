2eb599566a11934e07ca83fad03bf9dac1db34ff
Sung Ye Sol <yessol9096@naver.com>
2018-09-13 18:55:50 +0900

    drill 04 완성
    2015182014 성예솔
    https://www.youtube.com/watch?v=mQJCBepQvdM&feature=share

--- Drills/Drill04/character_runs.py
+++ Drills/Drill04/character_runs.py
@@ -1,12 +1,37 @@
 from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('run_animation.png')
+character = load_image('animation_sheet.png')
 
 x = 0
-
+frame = 0
+dir = 0
 # 여기를 채우세요.
-
+while (1) :
+    if(dir == 0 ):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100 , 100, 100, x, 90 )
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += 5
+        delay(0.05)
+        get_events()
+        if ( x > 800 ):
+            dir = 1
+    elif(dir == 1 ):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0 , 100, 100, x, 90 )
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        delay(0.05)
+        get_events()
+        if ( x < 0 ):
+            dir = 0
 
 close_canvas()
+
+
 
