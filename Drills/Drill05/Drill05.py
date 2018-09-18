fa6de737ab0ac43451b44974bd7e01f7499922c6
Sung Ye Sol <yessol9096@naver.com>
2018-09-18 17:49:25 +0900

    코드의 기본 구조 완성
    무한 반복하면서 네모를 그리고 원을 그리는 것의 기본 구조 완성

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -6,6 +6,15 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 # 여기를 채우세요.
+def make_rectangle():
+    pass
+
+def make_circle():
+    pass
+
+while True:
+    make_rectangle()
+    make_circle()
 
 close_canvas()
 
