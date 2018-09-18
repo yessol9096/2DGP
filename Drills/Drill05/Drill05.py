35848b5d7b538ce94659a11e6c0e992ccbfea93f
Sung Ye Sol <yessol9096@naver.com>
2018-09-18 19:08:22 +0900

    부호 while 오류해결

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -8,13 +8,14 @@ character = load_image('animation_sheet.png')
 def go_to(sx, sy, ex, ey):
     if (sx < ex and sy < ey):
         move_right_up(sx, sy, ex, ey)
-    elif (sx < ex and sy > ey):
+    if (sx < ex and sy > ey):
         move_right_down(sx, sy, ex, ey)
-    elif (sx > ex and sy < ey):
+    if (sx > ex and sy < ey):
         move_left_up(sx, sy, ex, ey)
-    elif (sx > ex and sy > ey):
+    if (sx > ex and sy > ey):
         move_left_down(sx, sy, ex, ey)
-    pass
+
+
 
 def move_right_up(sx, sy, ex, ey):
     frame = 0
@@ -29,11 +30,11 @@ def move_right_up(sx, sy, ex, ey):
         if(sy < ey):
             sy += 2
         delay(0.01)
-        if(sx > ex and sy > ey):
-            break;
+        if((sx > ex or sx == ex) and (sy > ey or sy == ey)):
+            break
     delay(0.05)
     get_events()
-    pass
+
 def move_right_down(sx, sy, ex, ey):
     frame = 0
     while True:
@@ -47,11 +48,10 @@ def move_right_down(sx, sy, ex, ey):
         if (sy > ey):
             sy -= 2
         delay(0.01)
-        if (sx > ex and sy < ey):
-            break;
+        if ((sx > ex or sx == ex)and (sy < ey or sy == ey)):
+            break
     delay(0.05)
     get_events()
-    pass
 def move_left_up(sx, sy, ex, ey):
     frame = 0
     while True:
@@ -65,8 +65,8 @@ def move_left_up(sx, sy, ex, ey):
         if (sy < ey):
             sy += 2
         delay(0.01)
-        if (sx < ex and sy > ey):
-            break;
+        if ((sx < ex or sx == ex)and (sy > ey or sy == ey)):
+            break
     delay(0.05)
     get_events()
 
@@ -83,22 +83,21 @@ def move_left_down(sx, sy, ex, ey):
         if (sy > ey):
             sy -= 2
         delay(0.01)
-        if (sx < ex and sy < ey):
-            break;
+        if ((sx < ex or sx == ex)and (sy < ey or sy == ey)):
+            break
     delay(0.05)
     get_events()
 
 while True:
-    move_left_down(132, 243, 35, 0)
-    #go_to(203, 535, 132, 243)
-    #go_to(132, 243, 535, 470)
-    #go_to(535, 470, 477, 203)
-    #go_to(477, 203, 715, 136)
-    #go_to(715, 136, 316, 225)
-    #go_to(316, 255, 510, 92)
-    #go_to(510, 92, 692, 518)
-    #go_to(692, 518, 682, 336)
-    #go_to(682, 336, 712, 349)
-    #go_to(712, 349, 203, 535)
+    go_to(203, 535, 132, 243)
+    go_to(132, 243, 535, 470)
+    go_to(535, 470, 477, 203)
+    go_to(477, 203, 715, 136)
+    go_to(715, 136, 316, 225)
+    go_to(316, 255, 510, 92)
+    go_to(510, 92, 692, 518)
+    go_to(692, 518, 682, 336)
+    go_to(682, 336, 712, 349)
+    go_to(712, 349, 203, 535)
 close_canvas()
 
