fa72e8e2aa7cf3d47726c856850ac9c576ac3a2d
Sung Ye Sol <yessol9096@naver.com>
2018-09-18 18:08:49 +0900

    위로 올라가는 함수 완성
    위로 올라가는 함수 완성

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -16,7 +16,14 @@ def move_from_center_to_right():
         delay(0.01)
 
 def move_up():
-    pass
+    x, y = 800 - 25 // 2, 50 + 40
+    while y < 600 - 50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y += 2
+        delay(0.01)
+
 def move_left():
     pass
 def move_down():
@@ -24,7 +31,7 @@ def move_down():
 def move_right_to_center():
     pass
 def make_rectangle():
-    move_from_center_to_right()
+    #move_from_center_to_right()
     move_up()
     move_left()
     move_down()
