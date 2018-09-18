a5aee21aa25dd039d69d8457f3ce7ff2708ff7ab
Sung Ye Sol <yessol9096@naver.com>
2018-09-18 18:56:50 +0900

    move_right_down함수완성

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -35,6 +35,22 @@ def move_right_up(sx, sy, ex, ey):
     get_events()
     pass
 def move_right_down(sx, sy, ex, ey):
+    frame = 0
+    while True:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, sx, sy)
+        update_canvas()
+        frame = (frame + 1) % 8
+        if (sx < ex):
+            sx += 2
+        if (sy > ey):
+            sy -= 2
+        delay(0.01)
+        if (sx > ex and sy < ey):
+            break;
+    delay(0.05)
+    get_events()
     pass
 def move_left_up(sx, sy, ex, ey):
     pass
@@ -42,7 +58,7 @@ def move_left_down(sx, sy, ex, ey):
     pass
 
 while True:
-    move_right_up(132, 243, 535, 470)
+    move_right_down(132, 243, 535, 100)
     #go_to(203, 535, 132, 243)
     #go_to(132, 243, 535, 470)
     #go_to(535, 470, 477, 203)
