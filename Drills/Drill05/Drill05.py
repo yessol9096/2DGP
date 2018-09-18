307f5c70c1d7d7046229caa34b7b1a506f47cfb2
Sung Ye Sol <yessol9096@naver.com>
2018-09-18 18:55:35 +0900

    move_right_up함수 완성

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -17,6 +17,22 @@ def go_to(sx, sy, ex, ey):
     pass
 
 def move_right_up(sx, sy, ex, ey):
+    frame = 0
+    while True:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, sx, sy)
+        update_canvas()
+        frame = (frame + 1) % 8
+        if(sx < ex):
+            sx += 2
+        if(sy < ey):
+            sy += 2
+        delay(0.01)
+        if(sx > ex and sy > ey):
+            break;
+    delay(0.05)
+    get_events()
     pass
 def move_right_down(sx, sy, ex, ey):
     pass
@@ -26,15 +42,16 @@ def move_left_down(sx, sy, ex, ey):
     pass
 
 while True:
-    go_to(203, 535, 132, 243)
-    go_to(132, 243, 535, 470)
-    go_to(535, 470, 477, 203)
-    go_to(477, 203, 715, 136)
-    go_to(715, 136, 316, 225)
-    go_to(316, 255, 510, 92)
-    go_to(510, 92, 692, 518)
-    go_to(692, 518, 682, 336)
-    go_to(682, 336, 712, 349)
-    go_to(712, 349, 203, 535)
+    move_right_up(132, 243, 535, 470)
+    #go_to(203, 535, 132, 243)
+    #go_to(132, 243, 535, 470)
+    #go_to(535, 470, 477, 203)
+    #go_to(477, 203, 715, 136)
+    #go_to(715, 136, 316, 225)
+    #go_to(316, 255, 510, 92)
+    #go_to(510, 92, 692, 518)
+    #go_to(692, 518, 682, 336)
+    #go_to(682, 336, 712, 349)
+    #go_to(712, 349, 203, 535)
 close_canvas()
 
