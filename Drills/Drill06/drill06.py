2ac5fedbef8e0b485ade1d855402ca652e8fea26
Sung Ye Sol <yessol9096@naver.com>
2018-09-20 18:33:34 +0900

    뼈대 변경
    커서 그리는 함수 추가

--- Drills/Drill06/drill06.py
+++ Drills/Drill06/drill06.py
@@ -2,15 +2,12 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-def get_mouseXY():
-    pass
-def go_to():
-    pass
 
 def handle_events():
     # fill here
     global running
     global x, y
+    global Click
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -20,21 +17,26 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-
-
+def go_to():
+    pass
+def draw_cursor():
+    pass
 # fill here
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+cursor = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
+Click = 0
 hide_cursor()
 
 while running:
-    get_mouseXY()
-    go_to()
+    draw_cursor()
+    if(Click):
+        go_to()
 
 close_canvas()
 
