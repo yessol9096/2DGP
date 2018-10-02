1e765cf3f425be1cc06e51d89d55ca46c454d872
Sung Ye Sol <yessol9096@naver.com>
2018-10-02 18:47:19 +0900

    화면 에 그림 띄우기

--- Drills/Drill07/drill07.py
+++ Drills/Drill07/drill07.py
@@ -1,11 +1,23 @@
-import turtle
+from pico2d import *
 import random
 
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 def draw_line(p1, p2):
+    frame = 0
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    hide_cursor()
+    delay(0.05)
     pass
 
-
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
 
 size = 20
 points = [(random.randint(100, 1180), random.randint(100, 980)) for i in range(size)]
