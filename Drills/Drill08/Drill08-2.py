d85456a54b3477fb8c5c5780e74dbf46df18fd0d
Sung Ye Sol <yessol9096@naver.com>
2018-10-04 18:30:33 +0900

    범위 수정

--- Drills/Drill08/Drill08-2.py
+++ Drills/Drill08/Drill08-2.py
@@ -30,7 +30,7 @@ def draw_curve_10_points(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
         frame = (frame + 1) % 8
         hide_cursor()
         delay(0.05)
-    for i in range(0, 50, 2):
+    for i in range(0, 100, 2):
         t = i / 100
         clear_canvas()
         kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
@@ -44,7 +44,7 @@ def draw_curve_10_points(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
         frame = (frame + 1) % 8
         hide_cursor()
         delay(0.05)
-    for i in range(0, 50, 2):
+    for i in range(0, 100, 2):
         t = i / 100
         clear_canvas()
         kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
@@ -58,7 +58,7 @@ def draw_curve_10_points(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
         frame = (frame + 1) % 8
         hide_cursor()
         delay(0.05)
-    for i in range(0, 50, 2):
+    for i in range(0, 100, 2):
         t = i / 100
         clear_canvas()
         kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
@@ -72,7 +72,32 @@ def draw_curve_10_points(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
         frame = (frame + 1) % 8
         hide_cursor()
         delay(0.05)
-
+    for i in range(0, 100, 2):
+        t = i / 100
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        draw_big_point(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10)
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p6[0] + (t ** 3 - t ** 2) * p7[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p6[1] + (t ** 3 - t ** 2) * p7[1]) / 2
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        hide_cursor()
+        delay(0.05)
+    for i in range(0, 50, 2):
+        t = i / 100
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        draw_big_point(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10)
+        x = (2 * t ** 2 - 3 * t + 1) * p5[0] + (-4 * t ** 2 + 4 * t) * p6[0] + (2 * t ** 2 - t) * p7[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p5[1] + (-4 * t ** 2 + 4 * t) * p6[1] + (2 * t ** 2 - t) * p7[1]
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        hide_cursor()
+        delay(0.05)
 
 # fill here
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
