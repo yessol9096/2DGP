bd2b7b23bd31433639a6b07d94f7c20c9b857eff
Sung Ye Sol <yessol9096@naver.com>
2018-10-04 14:46:21 +0900

    drill08-1 무한찍기 commit4

--- Drills/Drill08/Drill08-1.py
+++ Drills/Drill08/Drill08-1.py
@@ -100,9 +100,10 @@ def draw_curve_4_points(p1, p2, p3, p4):
 
 
 prepare_turtle_canvas()
+points = [(-300, 200), (400, 350), (300, -300), (-200, -200)]
 
+while(1):
+    draw_curve_4_points(points[0], points[1], points[2], points[3])
 
-points = [(-300, 200),(400, 350),(300, -300),(-200, -200)]
-draw_curve_4_points(points[0], points[1], points[2], points[3])
 
 turtle.done()
