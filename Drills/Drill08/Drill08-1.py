1796bfc08b40f3cda205514c0ad6bbdaed5baea3
Sung Ye Sol <yessol9096@naver.com>
2018-10-04 14:41:59 +0900

    drill08-1 4개 까지 찍기 commit3

--- Drills/Drill08/Drill08-1.py
+++ Drills/Drill08/Drill08-1.py
@@ -88,6 +88,13 @@ def draw_curve_4_points(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p4)
 
+    for i in range(50, 100, 2):
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * p3[0] + (-4 * t ** 2 + 4 * t) * p4[0] + (2 * t ** 2 - t) * p1[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p3[1] + (-4 * t ** 2 + 4 * t) * p4[1] + (2 * t ** 2 - t) * p1[1]
+        draw_point((x, y))
+    draw_point(p1)
+
 
 
 
