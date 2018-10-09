bba1f7f1665df5126839871c481a4e1ed101b798
Sung Ye Sol <yessol9096@naver.com>
2018-10-09 15:38:28 +0900

    잔디, 소년 그리기 drill09 commit:1

---
+++ Drills/Drill09/Drill09.py
@@ -0,0 +1,39 @@
+from pico2d import *
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+
+open_canvas()
+
+boy = Boy()
+grass = Grass()
+
+running = True
+
+while running:
+    boy.update()
+
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
+
+    delay(0.05)
