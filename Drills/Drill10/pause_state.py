7407263cdcbe1b1aedf33d7772b28181915a1bba
Sung Ye Sol <yessol9096@naver.com>
2018-10-11 18:46:15 +0900

    pause 화면에서 main 으로 가기

--- Drills/Drill10/pause_state.py
+++ Drills/Drill10/pause_state.py
@@ -47,7 +47,8 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
-
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
 
 
 def update():
