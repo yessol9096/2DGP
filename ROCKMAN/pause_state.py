82cae0427a01c3040c65d66b2088a3f77c7fd712
Sung Ye Sol <yessol9096@naver.com>
2018-10-17 19:54:39 +0900

    gameframework 추가

---
+++ ROCKMAN/pause_state.py
@@ -0,0 +1,65 @@
+import random
+import json
+import os
+
+from pico2d import *
+
+import game_framework
+import title_state
+import main_state
+
+
+name = "MainState"
+
+pause_image = None
+
+
+class Pause:
+    def __init__(self):
+        self.image = load_image('pause.png')
+    def draw(self):
+        self.image.draw_now(400, 300, 200, 200)
+
+
+
+def enter():
+    global pause_image
+    pause_image = Pause()
+
+
+def exit():
+    pass
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
+
+
+def update():
+    pass
+
+
+def draw():
+    clear_canvas()
+    pause_image.draw()
+    update_canvas()
+
+
+
+
+
+
