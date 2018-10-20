f958eba694f79f94326126b1ae87e18c821ec02b
yessol9096 <yessol9096@naver.com>
2018-10-20 21:35:29 +0900

    player 이미지 띄우기

--- ROCKMAN/mygame.py
+++ ROCKMAN/mygame.py
@@ -5,7 +5,8 @@ import start_state
 import pause_state
 import advanced_pause_state
 import main_state
+import player
 pico2d.open_canvas()
-game_framework.run(main_state)
+game_framework.run(player)
 pico2d.close_canvas()
 # fill here
