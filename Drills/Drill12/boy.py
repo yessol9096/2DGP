442294dc1f013971f647bc6ba49e8a0dc34e1dca
Sung Ye Sol <yessol9096@naver.com>
2018-10-29 23:15:34 +0900

    drill12 10초 경과시 눕기

--- Drills/Drill12/boy.py
+++ Drills/Drill12/boy.py
@@ -46,7 +46,7 @@ class IdleState:
             boy.velocity -= RUN_SPEED_PPS
         elif event == LEFT_UP:
             boy.velocity += RUN_SPEED_PPS
-        boy.timer = 1000
+        boy.timer = get_time()
 
     @staticmethod
     def exit(boy, event):
@@ -57,10 +57,9 @@ class IdleState:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-        boy.timer -= 1
-        if boy.timer == 0:
+        if boy.timer + 10.0 <= get_time():
             boy.add_event(SLEEP_TIMER)
-
+        
     @staticmethod
     def draw(boy):
         if boy.dir == 1:
